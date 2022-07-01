3af7135bcaf9257c446def692c41135e1eebe57c
bumang <umangabhattarai11@gmail.com>
2022-07-01 13:15:49 +0545

    created nav bar and a banner

---
+++ src/components/HomeScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Banner from "./Banner";
+import NavBar from "./NavBar";
+
+const HomeScreen = () => {
+	return (
+		<div className="HomeScreen-Wrapper">
+			<NavBar />
+			<Banner />
+			{/* Row */}
+		</div>
+	);
+};
+
+export default HomeScreen;
